4db6ad69ce231c47073e426bf9a3aad636ef5aa5
jcpantunes@gmail.com <jcpantunes@gmail.com@026263d0-d9ed-7f9f-659d-717ee00971c6>
2014-08-11 03:33:53 +0000

    pf camel

--- trunk/pf/pp-camel-ws/src/main/java/org/apache/camel/example/cxf/ObterListaExcecaoIn.java
+++ trunk/pf/pp-camel-ws/src/main/java/org/apache/camel/example/cxf/ObterListaExcecaoIn.java
@@ -1,17 +1,29 @@
 package org.apache.camel.example.cxf;
 
+import java.util.ArrayList;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMSource;
+
 import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.Processor;
+import org.apache.cxf.message.MessageContentsList;
 
 public class ObterListaExcecaoIn implements Processor {
 
 	@Override
 	public void process(Exchange e) throws Exception {
+		Message message = e.getIn();
+    	MessageContentsList lista = (MessageContentsList) message.getBody();
+    	
+    	Source source = new DOMSource();
 		String resultado = e.getIn().getBody(String.class);
 		System.out.println("===> " + resultado);
-		// String xml = "<consultarListaExcecao></consultarListaExcecao>";
-		String xml = "<consultarListaExcecao />";
-		e.getOut().setBody(xml);
+		String xml = "";
+		// xml = "<consultarListaExcecao />";
+		// e.getIn().setBody(source);
+		e.getOut().setBody(new ArrayList());
 	}
 
 }
--- trunk/pf/pp-camel-ws/src/main/java/org/apache/camel/example/cxf/ObterListaExcecaoOut.java
+++ trunk/pf/pp-camel-ws/src/main/java/org/apache/camel/example/cxf/ObterListaExcecaoOut.java
@@ -1,12 +1,17 @@
 package org.apache.camel.example.cxf;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.Processor;
+import org.apache.cxf.message.MessageContentsList;
 
 public class ObterListaExcecaoOut implements Processor {
 
 	@Override
 	public void process(Exchange e) throws Exception {
+		Message message = e.getIn();
+    	// MessageContentsList lista = (MessageContentsList) message.getBody();
+    	
 		String resultado = e.getIn().getBody(String.class).toString();
 		System.out.println("===> " + resultado);
 	}
